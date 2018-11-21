658b2a446383ee93b187391b9480fc599011b3f4
jangjaehong <nuggets997@gmail.com>
2018-11-21 19:40:33 +0900

    add input_page.html

--- algorithm/chatbot.py
+++ algorithm/chatbot.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 from algorithm.model import Seq2Seq
+from algorithm.util import DataUtil
 
 def _get_answer(question_msg):
+    datautil = DataUtil()
+
     tf.reset_default_graph()
     with tf.Session() as sess:
         model = Seq2Seq(mode='inference')
         model.build()
-        question_msg = 'Hi'
         predict = model.prediction(sess, question_msg, load_ckpt=model.ckpt_dir + f'epoch_{model.n_epoch}')
-    print(predict)
+
+    print(datautil.idx2sent(predict, datautil.dec_reverse_vocab))
     return predict
 
 
-_get_answer("Hi")
 
