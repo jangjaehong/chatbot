f87bb7c3c7cf8ac4eb7a33e8bed474e0720013c2
jangjaehong <nuggets997@gmail.com>
2018-12-30 23:17:51 +0900

    update css

--- medibot/views.py
+++ medibot/views.py
@@ -325,11 +325,11 @@ def day_measure(request):
                     WHRReport(uid=uid, gender=gender, waist=waist, hip=hip, whr=whr_result, state=whr_state, pub_date=pub_date).save()
                     EnergyReport(uid=uid, gender=gender, age=age, stature=stature, weight=weight, energy=energy_result, state=energy_state, pub_date=pub_date).save()
 
-                    answer = "%s님 건강체크 결과입니다. \r\n"\
-                               "체질량지수: %d / %s \r\n"\
-                               "복부비만도: %d / %s \r\n"\
-                               "기초대사량: %d / %s"\
-                               % (request.user.username, bmi_result, bmi_state, whr_result, whr_state, energy_result, energy_state)
+                    answer = '''%s님 건강체크 결과입니다. \r\n 
+                    체질량지수: %d / %s \r\n
+                    복부비만도: %d / %s \r\n
+                    기초대사량: %d / %s'''\
+                             % (request.user.username, bmi_result, bmi_state, whr_result, whr_state, energy_result, energy_state)
                     ChatReport(uid=uid, speaker='com', username='Medi-BOT', contents=answer, pub_date=timezone.now()).save()
                     # 리턴값
                     context = {"result": 1, "speaker": 'com', 'message': answer, 'func': "", 'name': 'Medi-BOT',
